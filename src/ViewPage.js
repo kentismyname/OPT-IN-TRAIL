30f4ecc882c18ee5e1baf352814291b3edc0e05a
John Kenneth Dalisay <johnkennethdalisay9@gmail.com>
2025-05-18 23:19:50 +0800

    almost done

--- src/ViewPage.js
+++ src/ViewPage.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; // (Optional) Setup icon library properly
-import { faCamera } from '@fortawesome/free-solid-svg-icons';
+import { faCamera, faEnvelope, faEyeSlash, faFileArrowDown, faLocationDot, faShieldHalved, faTrash } from '@fortawesome/free-solid-svg-icons';
 import { faCopy } from '@fortawesome/free-solid-svg-icons';
 import { faMessage } from '@fortawesome/free-solid-svg-icons';
 import { faRectangleList } from '@fortawesome/free-solid-svg-icons';
@@ -13,11 +13,21 @@ import { faUser } from '@fortawesome/free-solid-svg-icons';
 import { faWrench } from '@fortawesome/free-solid-svg-icons';
 import { faGear } from '@fortawesome/free-solid-svg-icons';
 import { faFileCode } from '@fortawesome/free-solid-svg-icons';
+import { faKey } from '@fortawesome/free-solid-svg-icons';
+import { faCalendarDays } from '@fortawesome/free-solid-svg-icons';
+import { faPrint } from '@fortawesome/free-solid-svg-icons';
+import { faFileExport } from '@fortawesome/free-solid-svg-icons';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
 
 
 
 
-const SHEET_API_URL = 'https://script.google.com/macros/s/AKfycbyTrdBUCs897m8LpvXOTlekpIoKxge7SFc013dPyGrwRS2Rn_w4mjMFTwD65htRNctT/exec';
+
+
+
+
+
+const SHEET_API_URL = 'https://script.google.com/macros/s/AKfycby9GkXjXnZfz9Wln6VgTQgpeIrbSXkx1KSZkoTIn3dgEbd92gRxoQodi68JeYzhey_g/exec';
 
 function ViewPage() {
   const { rowIndex } = useParams();
@@ -47,8 +57,16 @@ function ViewPage() {
   const renderDetail = (label, keyOrValue, isRawValue = false) => (
     <div className="py-2 col-12">
       <div className='row'>
-        <div className="col-12" style={{ backgroundColor: '#eceff4', height: '30px' }}>{label}</div>
-        <div className='mt-1'><span>{isRawValue ? keyOrValue : data[keyOrValue]}</span></div>
+        <div className="col-12" style={{ backgroundColor: '#eceff4', height: '45px', padding: '10px' }}>{label}</div>
+        <div className='mt-1' style={{height: '25px', padding: '5px 10px'}}><span>{isRawValue ? keyOrValue : data[keyOrValue]}</span></div>
+      </div>
+    </div>
+  );
+  const renderDetail1 = (label, keyOrValue, isRawValue = false) => (
+    <div className="py-1 col-12" style={{fontSize: "18px"}}>
+      <div className='row'>
+        <div className="col-5" >{label}</div>
+        <div className='col-7' style={{marginLeft: "-80px"}}><span>{isRawValue ? keyOrValue : data[keyOrValue]}</span></div>
       </div>
     </div>
   );
@@ -59,62 +77,78 @@ function ViewPage() {
       <div className="row">
         
         <div className='col-md-1 bg-dark me-3'>
-          <div className='row text-white' style={{fontSize: "14px"}}>
-            <span className='mt-2'><FontAwesomeIcon icon={faCamera}style={{ color: '#9ca2a7' }}  /> Media</span>
-            <span className='mt-2'><FontAwesomeIcon icon={faCopy} style={{ color: '#9ca2a7' }} /> Pages</span>
-            <span className='mt-2'><FontAwesomeIcon icon={faMessage} style={{ color: '#9ca2a7' }} /> Comments</span>
-            <span className='mt-2'><FontAwesomeIcon icon={faRectangleList} style={{ color: '#9ca2a7' }} /> Feedback</span>
-            <span className='mt-2' style={{backgroundColor: "#2271b1"}}><FontAwesomeIcon icon={faNewspaper} style={{ color: '#9ca2a7' }} /> WPForms</span>
+          <div className='row text-white' style={{fontSize: "18px"}}>
+            <span className='mt-3'><FontAwesomeIcon icon={faCamera}style={{ color: '#9ca2a7' }}  /> Media</span>
+            <span className='mt-3'><FontAwesomeIcon icon={faCopy} style={{ color: '#9ca2a7' }} /> Pages</span>
+            <span className='mt-3'><FontAwesomeIcon icon={faMessage} style={{ color: '#9ca2a7' }} /> Comments</span>
+            <span className='mt-3'><FontAwesomeIcon icon={faRectangleList} style={{ color: '#9ca2a7' }} /> Feedback</span>
+            <span className='mt-3' style={{backgroundColor: "#2271b1"}}><FontAwesomeIcon icon={faNewspaper} style={{ color: '#9ca2a7' }} /> WPForms</span>
           </div>
 
-          <div className='row' style={{backgroundColor: "#2c3338", color: "#e5e9f0", fontSize: "13px"}}>
-            <span className='mt-2'>All Forms</span>
-            <span className='mt-2'>Add New Form</span>
-            <strong className='mt-2' style={{color: "White"}}>Entries</strong>
-            <span className='mt-2'>Payments</span>
-            <span className='mt-2'>Form Templates</span>
-            <span className='mt-2'>Settings</span>
-            <span className='mt-2'>Tools</span>
-            <span className='mt-2' style={{color: "orange"}}>Addons</span>
-            <span className='mt-2'>Analytics</span>
-            <span className='mt-2'>SMTP</span>
-            <span className='mt-2'>About Us</span>
-            <span className='mt-2'>Community</span>
+          <div className='row' style={{backgroundColor: "#2c3338", color: "#e5e9f0", fontSize: "15px"}}>
+            <span className='mt-3'>All Forms</span>
+            <span className='mt-3'>Add New Form</span>
+            <strong className='mt-3' style={{color: "White"}}>Entries</strong>
+            <span className='mt-3'>Payments</span>
+            <span className='mt-3'>Form Templates</span>
+            <span className='mt-3'>Settings</span>
+            <span className='mt-3'>Tools</span>
+            <span className='mt-3' style={{color: "orange"}}>Addons</span>
+            <span className='mt-3'>Analytics</span>
+            <span className='mt-3'>SMTP</span>
+            <span className='mt-3'>About Us</span>
+            <span className='mt-3'>Community</span>
           </div>
-          <div className='row text-white' style={{fontSize: "14px"}} >
-            <span className='mt-2'><FontAwesomeIcon icon={faPaintbrush} rotation={180} /> Apperance</span>
-            <span className='mt-2'><FontAwesomeIcon icon={faPlug} rotation={50} /> Plugins</span>
-            <span className='mt-2'><FontAwesomeIcon icon={faUser} /> Users</span>
-            <span className='mt-2'><FontAwesomeIcon icon={faWrench} /> Tools</span>
-            <span className='mt-2'><FontAwesomeIcon icon={faGear} /> Settings</span>
-            <span className='mt-2'><FontAwesomeIcon icon={faFileCode} />Code Snippets</span>
+          <div className='row text-white' style={{fontSize: "18px"}} >
+            <span className='mt-3'><FontAwesomeIcon icon={faPaintbrush} rotation={180} style={{ color: '#9ca2a7'}} /> Apperance</span>
+            <span className='mt-3'><FontAwesomeIcon icon={faPlug} rotation={50} style={{ color: '#9ca2a7'}} /> Plugins</span>
+            <span className='mt-3'><FontAwesomeIcon icon={faUser} style={{ color: '#9ca2a7'}} /> Users</span>
+            <span className='mt-3'><FontAwesomeIcon icon={faWrench} style={{ color: '#9ca2a7'}} /> Tools</span>
+            <span className='mt-3'><FontAwesomeIcon icon={faGear} style={{ color: '#9ca2a7'}} /> Settings</span>
+            <span className='mt-3'><FontAwesomeIcon icon={faFileCode} style={{ color: '#9ca2a7'}} />Code Snippets</span>
           </div>
         </div>
 
 
-        <div className="col-md-6 border">
-          <h4 className="py-2 border-bottom mb-0">Braces Inquiry</h4>
+        <div className="col-md-7 border rounded shadow-sm">
+          <h4 className="py-3 border-bottom mb-0" style={{fontSize: "18px"}}>Braces Inquiry</h4>
 
           {renderDetail('Name', `${data['First Name']} ${data['Last Name']}`, true)}
           {renderDetail('Address', 'Address')}
           {renderDetail('City', 'City')}
           {renderDetail('State', 'State')}
           {renderDetail('Phone', 'Phone')}
-          {renderDetail('I agree to be contacted at this phone number', 'Consent')}
-          {renderDetail('Birth Date', 'BirthDate')}
+          {renderDetail('I agree to be contacted at this phone number', 'Yes', true)}
+          {renderDetail('Birth Date', 'Birthdate')}
           {renderDetail('Member ID', 'MemberID')}
-          {renderDetail('Insurance', 'Insurance')}
-          {renderDetail('Preferred Braces', 'Braces')}
+          {renderDetail('Insurance', 'Medicare' , true)}
+          {renderDetail('Preferred Braces', 'Brace Types')}
         </div>
 
-        <div className="col-md-3">
-          <div className="bg-light p-4 rounded shadow-sm">
-            <h6 className="mb-3 border-bottom" style={{ height: '30px' }}>Entry Details</h6>
-            <p><strong>Entry ID:</strong> #{100000 + parseInt(rowIndex)}</p>
-            <p><strong>Submitted:</strong> {new Date().toLocaleString()}</p>
-            <p><strong>Modified:</strong> {new Date().toLocaleString()}</p>
-            <p><strong>User:</strong> QA</p>
-            <p><strong>User IP:</strong> {data['User IP'] || 'N/A'}</p>
+        <div className="col-md-3 mx-4">
+          <div className="row">
+            <div className="bg-light p-3 rounded shadow-sm border">
+              <h5 className="mb-2 border-bottom" style={{ height: '35px' }}>Entry Details</h5>
+              {renderDetail1(<><FontAwesomeIcon icon={faKey} style={{ color: '#9ca2a7', marginRight: '10px' }} />Entry ID:</>,`${506216 + parseInt(rowIndex)}`,true)}
+              {renderDetail1(<><FontAwesomeIcon icon={faCalendarDays} style={{ marginRight: "10px" , fontSize: "15px" , color: '#9ca2a7'}} />Submitted:</>,'Submitted')}
+              {renderDetail1(<><FontAwesomeIcon icon={faCalendarDays} style={{ marginRight: "10px" , fontSize: "15px" , color: '#9ca2a7'}} />Modified:</>,'Modified')}
+              {renderDetail1(<><FontAwesomeIcon icon={faUser} style={{ marginRight: "10px" , fontSize: "15px" , color: '#9ca2a7'}} />User:</>, 'QA', true)}
+              {renderDetail1(<><FontAwesomeIcon icon={faLocationDot} style={{ marginRight: "10px" , fontSize: "15px" , color: '#9ca2a7'}} />User IP:</>, 'IP ADDRESS')}
+            </div>
+
+            <div className="bg-light p-3 rounded shadow-sm border mt-4" style={{fontSize: "18px"}}>
+                <h5 className="mb-2 border-bottom" style={{ height: '35px' }}>Actions</h5>
+              <div className="row" style={{color: "#0566ac"}}>
+                <span className='mt-2'><FontAwesomeIcon icon={faPrint} style={{ marginRight: "10px" , fontSize: "15px" , color: '#9ca2a7'}} /> Print</span>
+                <span className='mt-2'><FontAwesomeIcon icon={faFileExport} style={{ marginRight: "8px" , fontSize: "15px" , color: '#9ca2a7'}} /> Export (CSV)</span>
+                <span className='mt-2'><FontAwesomeIcon icon={faFileArrowDown} style={{ marginRight: "14px" , fontSize: "15px" , color: '#9ca2a7'}} /> Export (XLSX)</span>
+                <span className='mt-2'><FontAwesomeIcon icon={faEnvelope} style={{ marginRight: "10px" , fontSize: "15px" , color: '#9ca2a7'}} /> Resend Notifications</span>
+                <span className='mt-2'><FontAwesomeIcon icon={faStar} style={{ marginRight: "9px" , fontSize: "15px" , color: '#9ca2a7'}} /> Unstar</span>
+                <span className='mt-2'><FontAwesomeIcon icon={faEyeSlash} flip='horizontal' style={{ marginRight: "10px" , fontSize: "15px" , color: '#9ca2a7'}} /> Mark as Unread</span>
+                <span className='mt-2'><FontAwesomeIcon icon={faShieldHalved} style={{ marginRight: "10px" , fontSize: "15px" , color: '#9ca2a7'}} /> Mark as Spam</span>
+                <span className='mt-2'><FontAwesomeIcon icon={faTrash} style={{ marginRight: "10px" , fontSize: "15px" , color: '#9ca2a7'}} /> Delete Entry</span>
+              </div>
+            </div>
           </div>
         </div>
       </div>
